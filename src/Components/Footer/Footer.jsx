1f3bf9561085b8cad7cb609fe32e7417226b11b7
Kartik Buttan <2021btechaidskartik9829@poornima.edu.in>
2024-08-10 18:23:45 +0530

    fix

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css'
 const Footer = () => {
     return (
         <div className='footer'>
-            <p style={{fontWeight:'500', fontSize:'1em'}}> Learn 🌱 → Code 🚀 → Build 🔥 → Repeat 🎯</p>
+            <p className='footer-elemets'> Learn 🌱 → Code 🚀 → Build 🔥 → Repeat 🎯</p>
         </div>
     )
 }
