55688ceb019afebd27b802764e33c54e7a4020c1
Kartik Buttan <2021btechaidskartik9829@poornima.edu.in>
2024-09-18 22:48:21 +0530

    [FEATURE] Added Vercel Analytics !

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,15 @@ import Resume from "./Screens/Resume/Resume";
 import Navbar from "./Components/Navbar/Navbar";
 import Footer from "./Components/Footer/Footer";
 import ReactGA from "react-ga4";
+import { Analytics } from "@vercel/analytics/react"
+
 
 const App = () => {  
   ReactGA.initialize(import.meta.env.VITE_GOOGLE_ANALYTICS);
   return (
     <BrowserRouter>
       <Navbar />
+      <Analytics/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Project />} />
